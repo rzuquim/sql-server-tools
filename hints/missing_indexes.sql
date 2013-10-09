98b6f7600264fbb22fcd59c7f117acd1d6325f6f
Rafael Zuquim <rzuquim@inoa.com.br>
2013-10-09 15:30:15 -0300

    object_name should depend on database_id

--- hints/missing_indexes.sql
+++ hints/missing_indexes.sql
@@ -13,11 +13,11 @@ SELECT 'Table A' UNION
 SELECT 'Table B' -- ect
 
 ;WITH missing_indexes AS (
-  SELECT  [object_name] = Object_name(mid.object_id),
+  SELECT  [database] = db_name(mid.database_id),
+          [object_name] = Object_name(mid.object_id, mid.database_id),
           mid.equality_columns, mid.inequality_columns, mid.included_columns, 
           [avg_cost] = mis.avg_total_user_cost, 
-          mis.unique_compiles, 
-          [database] = db_name(mid.database_id),
+          mis.unique_compiles,
           [total_cost] = (mis.avg_total_user_cost * mis.unique_compiles), 
           mis.avg_user_impact
   FROM sys.dm_db_missing_index_details mid
@@ -27,9 +27,9 @@ SELECT 'Table B' -- ect
                ON mig.index_group_handle = mis.group_handle
   WHERE (db_name(mid.database_id) LIKE @database))
   
-SELECT *, custo_total_provavel = (total_cost - (total_cost * (avg_user_impact/100)))
+SELECT *, probable_cost = (total_cost - (total_cost * (avg_user_impact/100)))
 FROM missing_indexes
 WHERE avg_cost > 3
       AND object_name NOT IN (SELECT table_name FROM @ignore_tables)
-      --AND ifa.avg_user_impact > 30
+      --AND avg_user_impact > 30
 ORDER BY avg_cost DESC, object_name  
