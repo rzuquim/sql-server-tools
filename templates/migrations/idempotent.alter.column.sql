3b1d07dbff7edc6bd065f829ca8d3d6dbb3713e4
Leonardo Santos <lsantos@inoa.com.br>
2014-11-06 11:12:48 -0200

    translating and updating default parameter values of alter column

--- templates/migrations/idempotent.alter.column.sql
+++ templates/migrations/idempotent.alter.column.sql
@@ -1,12 +1,12 @@
 
 --========================================================================
--- Alterando coluna <column> a tabela <schema,,dbo>.<table>
+-- Altering <column,,> from <schema,,dbo>.<table,,>
 --========================================================================
 IF EXISTS (
-  SELECT * FROM SYS.COLUMNS WHERE NAME = N'<column>'
-  AND OBJECT_ID = OBJECT_ID(N'<schema,,dbo>.<table>')) 
+  SELECT top 1 1 FROM SYS.COLUMNS WHERE NAME = N'<column,,>'
+  AND OBJECT_ID = OBJECT_ID(N'<schema,,dbo>.<table,,>')) 
   BEGIN
-    ALTER TABLE [<schema,,dbo>].[<table>]
-      ALTER COLUMN [<column>] <type> <nullable,,NOT NULL>;
+    ALTER TABLE [<schema,,dbo>].[<table,,>]
+      ALTER COLUMN [<column,,>] <type,,> <nullable,,NOT NULL>;
   END 
 GO
